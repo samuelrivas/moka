37ff3ccadc11a3e7b9e6bdc5885c696407e2afab
Samuel Rivas <samuelrivas@gmail.com>
2012-07-07 23:05:04 +0200

    We can test that the original functions are being called now

--- test/unit/moka_fsm_proper.erl
+++ test/unit/moka_fsm_proper.erl
@@ -22,7 +22,7 @@
 -export([new/1, defined/1]).
 
 %%% Transitions
--export([replace/3]).
+-export([replace/3, call/2]).
 
 %%%===================================================================
 %%% FSM Callbacks
@@ -47,6 +47,13 @@ precondition(_From, _Target, _State, _Call) -> true.
 %% positives due to matching errors
 postcondition(new, defined, _StateData, _Call, _Res) -> true;
 postcondition(defined, defined, _StateData, _Call, _Res) -> true;
+postcondition(_From, _Target, State, {call, _Mod, call, [_M, FunSpec]}, Res) ->
+    case lists:member(FunSpec, State#state.replaced) of
+        true ->
+            Res = properly_moked;
+        false ->
+            expected_exception(Res)
+    end;
 
 postcondition(_From, _Target, _StateData, _Call, _Res) -> false.
 
@@ -59,24 +66,25 @@ next_state_data(_From, _Target, State, _Res, _Call) ->
 %%% States
 %%%===================================================================
 new(_) ->
-    [{defined, call_replace()}].
+    [{defined, call_replace()}
+     , {new, call_function()}].
 
 defined(_) ->
     [{defined, call_replace()}
-     , {defined, {call, io, format, ["noop~n"]}}].
+     , {defined, call_function()}].
 
 call_replace() ->
-    {call, ?MODULE, replace, [{var, moka}, dest_module(), funct_spec()]}.
+    {call, ?MODULE, replace, [{var, moka}, dest_module(), dest_funct_spec()]}.
+
+call_function() ->
+    {call, ?MODULE, call, [origin_module(), orig_fun_spec()]}.
 
 %%%===================================================================
 %%% Generators
 %%%===================================================================
 
-funct_spec() ->
-    Mod = dest_module(),
-    RealFuncts =
-        [Funct || Funct = {Name, _Arity} <- Mod:module_info(exports)
-                      , Name /= module_info],
+dest_funct_spec() ->
+    RealFuncts = get_exported(dest_module()),
 
     %% For now, you must modify this list manually if you modify the origin
     %% module
@@ -84,11 +92,24 @@ funct_spec() ->
 
     proper_types:elements(RealFuncts ++ FakeFuncts).
 
+orig_fun_spec() ->
+    proper_types:elements(get_exported(origin_module())).
+
+get_exported(Mod) ->
+    [Funct || Funct = {Name, _Arity} <- Mod:module_info(exports)
+                  , Name /= module_info].
+
 %%%===================================================================
 %%% Transitions
 %%%===================================================================
 replace(Moka, Mod, {FunctName, Arity}) ->
     moka:replace(Moka, Mod, FunctName, make_fun(Arity)).
+
+call(Module, {Funct, Arity}) ->
+    try erlang:apply(Module, Funct, make_args(Arity))
+    of Result -> {ok, Result}
+    catch Type:Reason -> {exception, Type, Reason, erlang:get_stacktrace()}
+    end.
 
 %%%===================================================================
 %%% Properties
@@ -129,4 +150,11 @@ make_fun(N) when N =:= 3 -> fun(_, _, _) -> return_term(N) end.
 
 return_term(N) -> {moked_function_with_arity, N}.
 
-get_fun_spec([_Moka, _Dest, FunSpec]) -> FunSpec.
+get_fun_spec([_Moka, _Dest, FunSpec]) -> FunSpec;
+get_fun_spec([_Dest, FunSpec]) -> FunSpec.
+
+make_args(N) -> lists:duplicate(N, fake_arg).
+
+expected_exception({exception, error, undef, _}) -> true;
+expected_exception({exception, error, {called, _Mod, _Line}, _}) -> true;
+expected_exception(_) -> false.
