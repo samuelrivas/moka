6fccaa3b492a86f732299de4035a10ece5acdf40
Samuel Rivas <samuelrivas@gmail.com>
2012-07-08 10:52:16 +0200

    Test that we can swap behaviour between two modules
    This is a softer version of testing that we can modify behaviour, so we can aim
    first for this

--- test/unit/moka_mod_utils_eunit.erl
+++ test/unit/moka_mod_utils_eunit.erl
@@ -19,10 +19,37 @@ get_forms_test_() ->
           moka_mod_utils:get_forms(FakeModule))
     ].
 
+swap_forms_test_() ->
+    Module1 = test_module(),
+    Module2 = test_module2(),
+    {setup,
+     fun() ->
+             Forms1 = moka_mod_utils:get_forms(Module1),
+             Forms2 = moka_mod_utils:get_forms(Module2),
+             {Forms1, Forms2}
+     end,
+     fun(_) -> ok end,
+     fun({Forms1, Forms2}) ->
+             {inorder,
+              [?_assertEqual({foo, bar}, {Module1:foo(), Module2:bar()})
+               , ?_assertError(undef, Module1:bar())
+               , ?_assertError(undef, Module2:foo())
+
+               , ?_test(moka_mod_utils:load_forms(Module1, Forms2))
+               , ?_test(moka_mod_utils:load_forms(Module2, Forms1))
+
+               , ?_assertEqual({bar, foo}, {Module1:foo(), Module2:bar()})
+               , ?_assertError(undef, Module2:bar())
+               , ?_assertError(undef, Module1:foo())
+              ]}
+     end}.
+
 %%%===================================================================
 %%% Internals
 %%%===================================================================
 test_module() -> moka_test_module.
+
+test_module2() -> moka_test_module2.
 
 %% We assume this module doesn't exist
 fake_module() -> moka_fake_mod.
---
+++ test/unit/moka_test_module2.erl
@@ -0,0 +1,7 @@
+%%% @doc This module is part of the moka_mod_utils_eunit test
+
+-module(moka_test_module2).
+
+-export([bar/0]).
+
+bar() -> bar.
