a9ca5f8a7a3a72f89ede33571694c26831b9d7ea
Samuel Rivas <samuelrivas@gmail.com>
2012-07-08 10:23:40 +0200

    First unit tests for get_forms

--- test/unit/moka_mod_utils_eunit.erl
+++ test/unit/moka_mod_utils_eunit.erl
@@ -5,12 +5,24 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get_beam_code_test_() ->
+    FakeModule = fake_module(),
     [?_assert(is_binary(moka_mod_utils:get_object_code(test_module())))
      , ?_assertThrow(
-          {cannot_get_object_code, moka_fake_mod},
-          moka_mod_utils:get_object_code(moka_fake_mod))].
+          {cannot_get_object_code, FakeModule},
+          moka_mod_utils:get_object_code(FakeModule))].
+
+get_forms_test_() ->
+    FakeModule = fake_module(),
+    [?_assert(is_list(moka_mod_utils:get_forms(test_module())))
+     , ?_assertThrow(
+          {cannot_get_object_code, FakeModule},
+          moka_mod_utils:get_forms(FakeModule))
+    ].
 
 %%%===================================================================
 %%% Internals
 %%%===================================================================
 test_module() -> moka_test_module.
+
+%% We assume this module doesn't exist
+fake_module() -> moka_fake_mod.
