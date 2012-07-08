00f90435220580c00abbb88020effb9a082d3338
Samuel Rivas <samuelrivas@gmail.com>
2012-07-08 09:49:36 +0200

    More unit tests for moka_mod_utils

--- test/unit/moka_mod_utils_eunit.erl
+++ test/unit/moka_mod_utils_eunit.erl
@@ -5,7 +5,10 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get_beam_code_test_() ->
-    [?_assert(is_binary(moka_mod_utils:get_object_code(test_module())))].
+    [?_assert(is_binary(moka_mod_utils:get_object_code(test_module())))
+     , ?_assertThrow(
+          {cannot_get_object_code, moka_fake_mod},
+          moka_mod_utils:get_object_code(moka_fake_mod))].
 
 %%%===================================================================
 %%% Internals
