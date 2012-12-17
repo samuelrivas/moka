336ba641fe68a5fae9aa30d87ccf550ccdad63a9
Samuel Rivas <samuelrivas@gmail.com>
2012-12-17 08:37:47 +0100

    PERF more ugly benchmarking
    It seems meck is horribly slow in the scenario of doing a lot of calls to a
    mocked method, need more investigation

--- test/benchmarks/moka_bench.erl
+++ test/benchmarks/moka_bench.erl
@@ -1,3 +1,27 @@
+%%% Copyright (c) 2012, Samuel Rivas <samuelrivas@gmail.com>
+%%% All rights reserved.
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions are met:
+%%%     * Redistributions of source code must retain the above copyright
+%%%       notice, this list of conditions and the following disclaimer.
+%%%     * Redistributions in binary form must reproduce the above copyright
+%%%       notice, this list of conditions and the following disclaimer in the
+%%%       documentation and/or other materials provided with the distribution.
+%%%     * Neither the name the author nor the names of its contributors may
+%%%       be used to endorse or promote products derived from this software
+%%%       without specific prior written permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%%% ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+%%% INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+%%% ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+%%% THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 %%% @doc Test moka performance
 %%%
 %%% We use meck as baseline for comparison purposes
@@ -14,7 +38,7 @@
 %%%_* API ==============================================================
 moka_create_destroy_bench(Times) ->
   sel_application:start_app(moka),
-  moka_mod_utils:restore_module(target_module()),
+  moka_mod_utils:restore_module(dummy_target_module()),
   Self = self(),
   spawn_link(
     fun() ->
@@ -42,23 +66,85 @@ meck_create_destroy_bench(Times) ->
     done -> ok
   end.
 
+unmocked_sequential_call(Times) ->
+  Self = self(),
+  spawn_link(
+    fun() ->
+        {Microsecs, _} = timer:tc(fun() -> do_call(Times) end),
+        io:format(
+          "~.2f cycles per milisec~n~.2f microsecs per cycle~n",
+          [Times*1000/Microsecs, Microsecs/Times]),
+        Self ! done
+    end),
+  receive
+    done -> ok
+  end.
+
+meck_sequential_call_bench(Times) ->
+  Self = self(),
+  Target = actual_target_module(),
+  meck:new(Target),
+  meck:expect(Target, call1, fun(_) -> baz end),
+  spawn_link(
+    fun() ->
+        {Microsecs, _} = timer:tc(fun() -> do_call(Times) end),
+        io:format(
+          "~.2f cycles per milisec~n~.2f microsecs per cycle~n",
+          [Times*1000/Microsecs, Microsecs/Times]),
+        Self ! done
+    end),
+  receive
+    done -> ok
+  end,
+  meck:unload().
+
+moka_sequential_call_bench(Times) ->
+  Self = self(),
+  Target = actual_target_module(),
+  Moka = moka:start(Target),
+  moka:replace(Moka, dest_module, call1, fun(_) -> baz end),
+  moka:load(Moka),
+  spawn_link(
+    fun() ->
+        {Microsecs, _} = timer:tc(fun() -> do_call(Times) end),
+        io:format(
+          "~.2f cycles per milisec~n~.2f microsecs per cycle~n",
+          [Times*1000/Microsecs, Microsecs/Times]),
+        Self ! done
+    end),
+  receive
+    done -> ok
+  end,
+  moka:stop(Moka).
+
 %%%_* Private Functions ================================================
 
 moka_power_cycle(0)     -> ok;
 moka_power_cycle(Times) ->
-  Moka = moka:start(target_module()),
+  Moka = moka:start(dummy_target_module()),
   moka:load(Moka),
   moka:stop(Moka),
   moka_power_cycle(Times - 1).
 
 meck_power_cycle(0)     -> ok;
 meck_power_cycle(Times) ->
-  meck:new(target_module()),
-  meck:unload(target_module()),
+  meck:new(dummy_target_module()),
+  meck:unload(dummy_target_module()),
   meck_power_cycle(Times - 1).
 
+do_call(0) -> ok;
+do_call(Times) ->
+  Target = actual_target_module(),
+  Target:call1(doncare),
+  do_call(Times -1).
+
 %% You need this module compiled with debug info in the code path
-target_module() -> eunit.
+dummy_target_module() -> eunit.
+
+actual_target_module() -> moka_bench_target.
+
+%% Relevant for moka
+dest_module() -> moka_bench_dest.
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
