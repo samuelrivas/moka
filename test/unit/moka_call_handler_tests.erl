12b1648b7a422c28a99957c2e17b0cd37d2f665b
Samuel Rivas <samuelrivas@gmail.com>
2013-01-04 22:35:15 +0100

    TEST The call handler needs a reference to a history server to start
    For the unit tests it is irrelevant, calls are asynchronous and they cannot
    fail, even if the history server doesn't exist, so we just pass an arbitrary
    atom

--- test/unit/moka_call_handler_tests.erl
+++ test/unit/moka_call_handler_tests.erl
@@ -73,7 +73,7 @@ parallel_test_() ->
 %%%-------------------------------------------------------------------
 handler_name() -> test_handler.
 
-start_handler(Fun) -> moka_call_handler:start_link(handler_name(), Fun).
+start_handler(Fun) -> moka_call_handler:start_link(handler_name(), Fun, none).
 
 stop_handler(Pid) ->
     moka_call_handler:stop(handler_name()),
